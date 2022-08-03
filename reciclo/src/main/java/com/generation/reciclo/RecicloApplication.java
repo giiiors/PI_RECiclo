3b5ca2ad9dd6bebea675cfc472e790e322c8564b
unknown <ferreira.ellenk@gmail.com>
2022-08-03 11:55:19 -0300

    PI - TemaModel

---
+++ reciclo/src/main/java/com/generation/reciclo/RecicloApplication.java
@@ -0,0 +1,13 @@
+package com.generation.reciclo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecicloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecicloApplication.class, args);
+	}
+
+}
---
+++ reciclo/src/test/java/com/generation/reciclo/RecicloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.reciclo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecicloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
