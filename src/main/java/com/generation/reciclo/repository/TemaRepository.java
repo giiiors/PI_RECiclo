947a8998c1c2e11f49f85120846a56b1608c3d16
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2022-09-19 10:12:22 -0300

    substituicao descricao por titulo para buscar postagens por tema

--- src/main/java/com/generation/reciclo/repository/TemaRepository.java
+++ src/main/java/com/generation/reciclo/repository/TemaRepository.java
@@ -2,10 +2,11 @@ package com.generation.reciclo.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.generation.reciclo.model.TemaModel;
 
 @Repository
 public interface TemaRepository extends JpaRepository<TemaModel, Long>{
-	public List<TemaModel> findAllByTituloContainingIgnoreCase(String titulo);
+	public List<TemaModel>findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
